83f2b5e6ffc977623ad1980e5df2a8b678614ed8
Stanley <stanleykywu@gmail.com>
2023-10-07 14:27:33 -0500

    new photo

--- pages/header/Header.jsx
+++ pages/header/Header.jsx
@@ -17,10 +17,10 @@ const Header = () => {
     <Container id='home' align='center'>
       <Grid container display={(isMobile) ? 'block' : 'flex'} direction={(isMobile) ? 'column' : "row"} justifyContent="center" paddingTop={12}> 
         <Grid item md={4} paddingTop={2}>
-          <Avatar alt='Stanley Wu' src="/static/me.webp" variant='circular' sx={{ width: '300px', height: '300px' }} style={{background: 'linear-gradient(to right bottom, #96C2DB, transparent)'}}/>
+          <Avatar alt='Stanley Wu' src="/static/headshot-2023.webp" variant='circular' sx={{ width: '300px', height: '300px' }}/>
           <CTA/>
         </Grid>
-        <Grid md={1}/>
+        <Grid md={1 }/>
         <Grid item md={7} textAlign="left">
           <h1 style={{ marginTop: 0, marginBottom: 10, fontSize: 40 }}>Stanley Wu</h1>
           
@@ -100,7 +100,7 @@ const Header = () => {
             I am a 1st year Ph.D. student at the University of Chicago <a href='https://sandlab.cs.uchicago.edu/' target="_blank" rel="noreferrer" style={{textDecoration: 'underline', color: 'black'}}>SAND Lab</a>, co-advised by <a href='https://people.cs.uchicago.edu/~ravenben/' target="_blank" rel="noreferrer" style={{textDecoration: 'underline', color: 'black'}}>Ben Zhao</a> and <a href='https://people.cs.uchicago.edu/~htzheng/' target="_blank" rel="noreferrer" style={{textDecoration: 'underline', color: 'black'}}>Heather Zheng</a>. 
           </p>
           <p align="left">
-            My primary academic interests lie in trustworthy machine learning, and I am currently focused on researching solutions to those affected by unethical AI/ML systems.
+            My primary academic interests lie in trustworthy/adversarial machine learning, and I am currently focused on researching solutions for people affected by AI/ML systems.
           </p>
           <p>
             I received my bachelors in computer science from Northeastern University, during which I was very fortunate to work with <a href='https://www.ccs.neu.edu/home/alina/' target="_blank" rel="noreferrer" style={{textDecoration: 'underline', color: 'black'}}>Alina Oprea</a>, and <a href='https://jonathan-ullman.github.io/' target="_blank" rel="noreferrer" style={{textDecoration: 'underline', color: 'black'}}>Jonathan Ullman</a>. I have held several industry positions in the past, mainly in software engineering and data science. Recently, I was a data scientist at <a href='https://www.klaviyo.com/' target="_blank" rel="noreferrer" style={{textDecoration: 'underline', color: 'black'}}>Klaviyo</a> and <a href='https://www.proofpoint.com/us' target="_blank" rel="noreferrer" style={{textDecoration: 'underline', color: 'black'}}>Proofpoint</a>.
