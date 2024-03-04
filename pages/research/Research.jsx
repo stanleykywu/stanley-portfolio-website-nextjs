d12ceb3d9b9210183ec01bc180a2ae1e45e82f8b
Stanley Wu <stanleykywu@gmail.com>
2024-03-03 22:03:28 -0600

    added nightshade

--- pages/research/Research.jsx
+++ pages/research/Research.jsx
@@ -17,8 +17,23 @@ const Research = () => {
           <div align="left">
             <h2 style={{ paddingTop: 50, marginBottom: 30 }}>Papers</h2>
           </div>
-          <Container align="left" style={{ padding: 0 }}>
+          <Container align="left" style={{ padding: 0, marginBottom: 50 }}>
             <Grid container display={(isMobile) ? 'block' : 'flex'} direction={(isMobile) ? 'column' : "row"} justifyContent="center">
+              <Grid item xs={2} md={1} justifyContent="left">
+                <strong>2024</strong>
+              </Grid>
+              <Grid item xs={10} md={11} style={{ marginBottom: "20px"}}>
+                <div>
+                  <strong>Prompt-Specific Poisoning Attacks on Text-to-Image Generative Models<br/>
+                  </strong>Shawn Shan, Wenxin Ding, Josephine Passananti, Stanley Wu, Haitao Zheng, Ben Y. Zhao<br/>
+                  <em>preprint</em><br/>
+                  <div style={{display: "flex", flexDirection: "row", alignItems: "center"}}>
+                  <Button variant='contained' disableElevation href='https://arxiv.org/abs/2310.13828' target='_blank' rel="noopener noreferrer" sx={{ display: 'flex', flexDirection: 'column', backgroundColor: '#B31A1B', maxWidth: '30px', marginTop: '10px' }} className='slight__arxivhover'>
+                        <SiArxiv size={30} style={{color: '#fff', maxWidth: '40'}}/>
+                      </Button>
+                  </div>
+                </div>
+              </Grid>
               <Grid item xs={2} md={1} justifyContent="left">
                 <strong>2023</strong>
               </Grid>
